661c4af314770cddc322470ac703ce8ad341d933
Devender Kumar <deku@comp-pcapek.imm.dtu.dk>
2019-03-20 20:28:45 +0100

    hardcoded1

--- lib/movisens_device_probe.dart
+++ lib/movisens_device_probe.dart
@@ -23,7 +23,7 @@ class MovisensProbe extends StreamProbe {
      userData = UserData(100, 180, Gender.male, 25, SensorLocation.chest,
         '88:6B:0F:CD:E7:F2', 'Sensor 02655');
      
-     print("inside measure")
+     print("inside measure");
 
   /* userData = new UserData(m.weight, m.height, m.gender, m.age,
       SensorLocation.chest, m.address, m.name);
