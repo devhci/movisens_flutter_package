110d192c5d27ffd9def2699a2d644fd3dcf55a07
Devender Kumar <deku@comp-pcapek.imm.dtu.dk>
2019-03-21 11:32:01 +0100

    added data fromate in movisens Datum

--- lib/movisens_datum.dart
+++ lib/movisens_datum.dart
@@ -1,6 +1,11 @@
 part of movisens;
 
 class MovisensDatum extends CARPDatum {
+
+  static const DataFormat CARP_DATA_FORMAT = DataFormat(NameSpace.CARP, MovisensSamplingPackage.MOVISENS);
+  DataFormat get format => CARP_DATA_FORMAT;
+
+
   String movisensTimestamp;
 
   MovisensDatum() : super();
